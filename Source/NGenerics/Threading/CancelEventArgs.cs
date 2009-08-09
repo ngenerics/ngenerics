562de2720dbe9668dd07fd925bb946629301f646
rhanekom <rhanekom@39bbdeee-833e-11de-8ff2-33b0a5e28824>
2009-08-09 09:02:24 +0000

    Added CancelEventArgs to the project.
    git-svn-id: http://ngenerics.googlecode.com/svn/trunk@70 39bbdeee-833e-11de-8ff2-33b0a5e28824

--- Source/NGenerics/Threading/CancelEventArgs.cs
+++ Source/NGenerics/Threading/CancelEventArgs.cs
@@ -1,3 +1,14 @@
+/*  
+  Copyright 2007-2009 The NGenerics Team
+ (http://code.google.com/p/ngenerics/wiki/Team)
+
+ This program is licensed under the GNU Lesser General Public License (LGPL).  You should 
+ have received a copy of the license along with the source code.  If not, an online copy
+ of the license can be found at http://www.gnu.org/copyleft/lesser.html. 
+*/
+
+#if (SILVERLIGHT)
+
 using System;
 
 namespace NGenerics.Threading
@@ -31,3 +42,5 @@ namespace NGenerics.Threading
         public bool Cancel { get; set; }
     }
 }
+
+#endif
