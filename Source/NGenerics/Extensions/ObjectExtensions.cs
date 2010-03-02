d82e447ac564059bbf1811455c90d86818574a49
simon.cropp <simon.cropp@39bbdeee-833e-11de-8ff2-33b0a5e28824>
2010-03-02 03:29:17 +0000

    minor doc changes
    git-svn-id: http://ngenerics.googlecode.com/svn/trunk@259 39bbdeee-833e-11de-8ff2-33b0a5e28824

--- Source/NGenerics/Extensions/ObjectExtensions.cs
+++ Source/NGenerics/Extensions/ObjectExtensions.cs
@@ -29,7 +29,7 @@ namespace NGenerics.Extensions
         /// <typeparam name="T">The type to convert the object into.</typeparam>
         /// <param name="value">The value to convert.</param>
         /// <returns>The converted value.</returns>
-        /// <exception cref="System.NotSupportedException">When no compatible type converter is found for the operation.</exception>
+        /// <exception cref="NotSupportedException">When no compatible type converter is found for the operation.</exception>
         /// <example>
         /// <code source="..\..\Source\Examples\ExampleLibraryCSharp\Extensions\ObjectExtensionsExamples.cs" region="ConvertTo" lang="cs" title="The following example shows how to use the ConvertTo method."/>
         /// <code source="..\..\Source\Examples\ExampleLibraryVB\Extensions\ObjectExtensionsExamples.vb" region="ConvertTo" lang="vbnet" title="The following example shows how to use the ConvertTo method."/>
@@ -44,6 +44,7 @@ namespace NGenerics.Extensions
         /// <summary>
         /// Serializes any Serializable Object to XMLString
         /// </summary>
+        /// <remarks>Uses <see cref="XmlSerializer"/>.</remarks>
         /// <typeparam name="T">The type of object to serialize.</typeparam>
         /// <param name="obj">The object to serialize.</param>
         /// <returns>A serialized representation of the object.</returns>
@@ -61,6 +62,7 @@ namespace NGenerics.Extensions
         /// <summary>
         /// Deserializes object from String
         /// </summary>
+        /// <remarks>Uses <see cref="XmlSerializer"/>.</remarks>
         /// <typeparam name="T">The type of object to deserialize.</typeparam>
         /// <param name="xml">The XML to deserialize the object from.</param>
         /// <returns>ready populated object</returns>
@@ -75,12 +77,12 @@ namespace NGenerics.Extensions
 
 
         /// <summary>
-        /// Perfomrs a deep copy the specified serializable object.
+        /// Performs a deep copy the specified serializable object.
         /// </summary>
         /// <typeparam name="T">The type of object to perform a deep copy on.</typeparam>
         /// <param name="obj">The object to perform a deep copy on.</param>
         /// <returns>A copy of the specified object.</returns>
-        /// <remarks>This method relies on serialization for a generic deep copy routine - if performance is needed, rather implement <see cref="ICloneable"/>.</remarks>
+        /// <remarks>This method relies on serialization (<see cref="BinaryFormatter"/>) for a generic deep copy routine - if performance is needed, rather implement <see cref="ICloneable"/>.</remarks>
         public static T DeepCopy<T>(this T obj) 
         {
             var formatter = new BinaryFormatter();
