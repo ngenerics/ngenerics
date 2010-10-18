12784494001bb55e0d2224768c2d7bd12bdc1bab
husayt@gmail.com <husayt@gmail.com@39bbdeee-833e-11de-8ff2-33b0a5e28824>
2010-10-18 19:17:10 +0000

    Added some Magic. you can see the collection being sorted now. still raw though
    git-svn-id: http://ngenerics.googlecode.com/svn/trunk@308 39bbdeee-833e-11de-8ff2-33b0a5e28824

---
+++ Source/Examples/WpfExample/DelayedObservableCollection.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace WpfExample
+{
+    public class DelayedObservableCollection<T> : ObservableCollection<T>
+    {
+        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> class.</summary>
+        public DelayedObservableCollection()
+            : base()
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> class that contains elements copied from the specified collection.</summary>
+        /// <param name="collection">The collection from which the elements are copied.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="collection" /> parameter cannot be null.</exception>
+        public DelayedObservableCollection(IEnumerable<T> collection)
+            : base(collection)
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Collections.ObjectModel.ObservableCollection`1" /> class that contains elements copied from the specified list.</summary>
+        /// <param name="list">The list from which the elements are copied.</param>
+        /// <exception cref="T:System.ArgumentNullException">The <paramref name="list" /> parameter cannot be null.</exception>
+        public DelayedObservableCollection(List<T> list)
+            : base(list)
+        {
+        }
+        protected override void SetItem(int index, T item)
+        {
+            base.SetItem(index, item);
+
+            Thread.Sleep(100);
+        }
+
+    }
+}
--- Source/Examples/WpfExample/MainWindow.xaml.cs
+++ Source/Examples/WpfExample/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Media;
 using NGenerics.Sorting;
@@ -49,8 +50,11 @@ namespace WpfExample
         public MainWindow()
         {
             InitializeComponent();
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
 
-            for (var i = 5; i > 0; i--)
+            for (var i = 1; i > 0; i--)
             {
                 var chrt = new HGChartCtrl
                             {
@@ -67,6 +71,8 @@ namespace WpfExample
                 hgc.Add(chrt);
             }
         }
+
+
 
         public void SetSource(ObservableCollection<int> src)
         {
@@ -85,7 +91,7 @@ namespace WpfExample
 
         private void AddRandomData()
         {
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 10; i++)
             {
                 _mainData.Add(_rnd.Next(250));
             }
@@ -99,26 +105,110 @@ namespace WpfExample
 
         private void SortChartData()
         {
+            ISorter<int> sorter = GetSorter();
+            if (sorter == null) return;
+
+            var sortedData = new ObservableCollection<int>(_mainData);
+            SetSource(sortedData);
+
+            sorter.Sort(sortedData, SortOrder.Ascending);
+
+        }
+
+        private ISorter<int> GetSorter()
+        {
             var sorter = comboSorter.SelectedItem as ISorter<int>;
 
             if (sorter == null)
             {
                 MessageBox.Show("Please, choose sorting method");
-                return;
             }
-
-            var sortedData = new ObservableCollection<int>(_mainData);
-            sorter.Sort(sortedData, SortOrder.Ascending);
-            SetSource(sortedData);
+            return sorter;
         }
 
         private void btnMagic_Click(object sender, RoutedEventArgs e)
         {
+            // backgroundworker is the most robust technique because it allows
+            //   monitoring and cancellation of task
+            //Status.Text = "Working using BackgroundWorker...";
+            StartBackgroundWorker();
+        }
 
+
+        #region The Work
+        // methods that do the time consuming work
+
+        private BackgroundWorker backgroundWorker = new BackgroundWorker
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true,
+        };
+
+        private void StartBackgroundWorker()
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                btnMagic.Content = "Start magic";
+                backgroundWorker.CancelAsync();
+            }
+            else
+            {
+                ISorter<int> sorter = GetSorter();
+                if (sorter == null) return;
+
+
+                btnMagic.Content = "Stop magic";
+                backgroundWorker.RunWorkerAsync(sorter);
+
+            }
+
+        }
+
+
+        // This method performs work on a separate thread
+        // (not UI thread). It does NOT update UI.
+        // UI is updated directly by the Background Completed method
+        // Used by: Background thread technique of executing work
+        //         
+        ObservableCollection<int> SortDataInDifferentThread(ISorter<int> sorter)
+        {
+            var sortedData = new DelayedObservableCollection<int>(_mainData);
+            sortedData.CollectionChanged += _sortData_CollectionChanged;
+            sorter.Sort(sortedData, SortOrder.Ascending);
+            return new ObservableCollection<int>(sortedData);
+        }
+
+        void _sortData_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            backgroundWorker.ReportProgress(1, sender);
+        }
+
+        // Runs on Ui thread
+        void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            SetSource(e.UserState as ObservableCollection<int>);
+        }
+
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // UI Update is done directly in Background completed method
+            e.Result = SortDataInDifferentThread(e.Argument as ISorter<int>);
+
+        }
+
+        ///runs on Ui Thread
+        void backgroundWorker_RunWorkerCompleted(object sender,
+            RunWorkerCompletedEventArgs e)
+        {
+            btnMagic.Content = "Start magic";
+            SetSource(e.Result as ObservableCollection<int>);
 
 
         }
 
+
+        #endregion
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             AddRandomData();
