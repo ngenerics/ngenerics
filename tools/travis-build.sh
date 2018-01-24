02dc97afd1a42bf230dc0b594b3c34d382e56a03
Riaan Hanekom <rhanekom@gmail.com>
2018-01-24 19:01:43 +0200

    script fixes

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -2,7 +2,7 @@
 cd src
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	mono tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:NGenerics /k:ngenerics-github /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /v:"2.0" 
+	mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe begin /n:NGenerics /k:ngenerics-github /d:sonar.login=${SONAR_TOKEN} /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /v:"2.0" 
 fi
 
 dotnet build
@@ -10,5 +10,5 @@ dotnet test NGenerics.Tests --logger:trx
 dotnet test NGenerics.Examples --logger:trx
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	mono tools/sonar/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
+	mono ../tools/sonar/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
 fi
